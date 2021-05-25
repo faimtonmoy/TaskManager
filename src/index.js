bccc69c21a147f9a7799d9255f0c3cc7fce3c615
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-05-25 22:02:27 +0600

    third commit

--- src/index.js
+++ src/index.js
@@ -13,22 +13,42 @@ app.post ('/users',(req, res)=>{
 
     const user= new User(req.body)
     user.save().then(()=>{
-           res.send(user)
+           res.status(201).send(user)
     }).catch((e)=>{
-        res.send(e)
+        res.status(400).send(e)
+    })
+})
+
+app.get('/users', (req,res)=>{
+    User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+
+        res.status(400).send(e)
+
+    })
+})
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+    User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send(e)
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
     })
 })
 app.post ('/tasks',(req, res)=>{
 
     const task= new Task(req.body)
     task.save().then(()=>{
-           res.send(task)
+           res.status(201).send(task)
     }).catch((e)=>{
-        res.send(e)
+        res.status(400).send(e)
     })
 })
-
-
 
 app.listen(port, ()=>{
     console.log("Server is running on port " + port)
