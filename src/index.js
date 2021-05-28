28461889df4f3082a7616553f4d4ef1101feda9c
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-05-28 20:40:41 +0600

    fourth commit

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.get('/users/:id', (req, res) => {
     const _id = req.params.id
     User.findById(_id).then((user) => {
         if(!user) {
-            return res.status(404).send(e)
+            return res.status(404).send()
         }
 
         res.send(user)
@@ -47,6 +47,26 @@ app.post ('/tasks',(req, res)=>{
            res.status(201).send(task)
     }).catch((e)=>{
         res.status(400).send(e)
+    })
+})
+app.get('/tasks',(req, res)=>{
+    Task.find({}).then((tasks)=>{
+      res.send(tasks)
+    }).catch((e)=>{
+        res.status(404).send(e)
+    })
+})
+
+app.get('/tasks/:id', (req, res)=>{
+    const _id= req.params.id
+    Task.findById(_id).then((task)=>{
+        if(!task)
+        {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send(e)
     })
 })
 
