b578e89340b28f7d2f5a26c5e7d4291eda271cea
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-06-05 22:25:16 +0600

    Routes added

---
+++ src/routers/task.js
@@ -0,0 +1,96 @@
+const express= require('express')
+const router= new express.Router()
+const Task= require('../models/tasks')
+
+router.post ('/tasks', async (req, res)=>{
+
+    const task= new Task(req.body)
+    try{
+       await task.save()
+       res.status(201).send(task)
+    } catch(e){
+       res.status(400).send(e)
+    }
+    /*task.save().then(()=>{
+           res.status(201).send(task)
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })*/
+})
+router.get('/tasks',async (req, res)=>{
+    try{
+      
+       const task= await Task.find({})
+       res.send(task)
+
+    } catch(e){
+       res.send(e)
+    }
+    /*Task.find({}).then((tasks)=>{
+      res.send(tasks)
+    }).catch((e)=>{
+        res.status(404).send(e)
+    })*/
+})
+
+router.get('/tasks/:id', async (req, res)=>{
+    const _id= req.params.id
+    try{
+        const task= await Task.findById(_id)
+        if(!task)
+        {
+            return res.status(404).send()
+        }
+        res.send(task)
+    } catch(e){
+        res.status(500).send(e)
+    }
+    /*Task.findById(_id).then((task)=>{
+        if(!task)
+        {
+            return res.status(404).send()
+        }
+        res.send(task)
+    }).catch((e)=>{
+        res.status(500).send(e)
+    })*/
+})
+router.patch('/tasks/:id', async(req, res)=>{
+    const updates= Object.keys(req.body)
+    const allowUpdates= ['Description', 'Completed']
+    const validOperation= updates.every((update)=> allowUpdates.includes(update))
+    if(!validOperation){
+        return res.status(400).send({error:'Invalid Value'})
+    }
+    try{
+        
+        const update= await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!update)
+        {
+            return res.status(404).send()
+        }
+        res.send(update)
+
+    }catch(e){
+         res.status(400).send(e)
+    }
+    
+
+})
+router.delete('/tasks/:id', async (req, res)=>{
+    try{
+       const task= await Task.findByIdAndDelete(req.params.id)
+       if(!task)
+       {
+           res.status(400).send()
+       }
+       res.send(task)
+    }catch(e){
+
+        res.status(404).send(e)
+
+    }
+
+})
+
+module.exports= router
---
+++ src/routers/user.js
@@ -0,0 +1,100 @@
+
+const express= require('express')
+const router= new express.Router()
+const User= require('../models/users')
+
+
+router.post ('/users', async (req, res)=>{
+
+    const user= new User(req.body)
+    /*user.save().then(()=>{
+           res.status(201).send(user)
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })*/
+    try{
+        await user.save()
+        res.status(201).send(user)
+    }catch(e){
+        res.status(400).send(e)
+    }
+})
+
+router.get('/users', async (req,res)=>{
+    try{
+        const user= await User.find({})
+        res.send(user)
+
+    }catch(e){
+        res.status(400).send(e)
+    }
+    /*User.find({}).then((users)=>{
+        res.send(users)
+    }).catch((e)=>{
+
+        res.status(400).send(e)
+
+    })*/
+})
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
+    try{
+
+        const user= await User.findById(_id)
+        if(!user) {
+            return res.status(404).send()
+        }
+        res.send(user)
+
+    } catch(e){
+        res.status(500).send(e)
+    }
+    /*User.findById(_id).then((user) => {
+        if(!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send(e)
+    })*/
+})
+router.patch('/users/:id', async(req, res)=>{
+    const updates= Object.keys(req.body)
+    const allowUpdates= ['name', 'email', 'password', 'age']
+    const validOperation= updates.every((update)=> allowUpdates.includes(update))
+    if(!validOperation){
+        return res.status(400).send({error:'Invalid Value'})
+    }
+    try{
+        
+        const update= await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!update)
+        {
+            return res.status(404).send()
+        }
+        res.send(update)
+
+    }catch(e){
+         res.status(400).send(e)
+    }
+    
+
+})
+router.delete('/users/:id', async (req, res)=>{
+    try{
+       const user= await User.findByIdAndDelete(req.params.id)
+       if(!user)
+       {
+           res.status(400).send()
+       }
+       res.send(user)
+    }catch(e){
+
+        res.status(404).send(e)
+
+    }
+
+})
+
+module.exports= router
