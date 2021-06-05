5b01f980c160c2f2a9938a69416f53844e4328b9
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-06-05 20:26:47 +0600

    Update Task Added

--- src/index.js
+++ src/index.js
@@ -139,7 +139,28 @@ app.get('/tasks/:id', async (req, res)=>{
         res.status(500).send(e)
     })*/
 })
-app.patch('/tasks/:id', async)
+app.patch('/tasks/:id', async(req, res)=>{
+    const updates= Object.keys(req.body)
+    const allowUpdates= ['Description', 'Completed']
+    const validOperation= updates.every((update)=> allowUpdates.includes(update))
+    if(!validOperation){
+        return res.status(400).send({error:'Invalid Value'})
+    }
+    try{
+        
+        const update= await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
+        if(!update)
+        {
+            return res.status(404).send()
+        }
+        res.send(update)
+
+    }catch(e){
+         res.status(400).send(e)
+    }
+    
+
+})
 
 app.listen(port, ()=>{
     console.log("Server is running on port " + port)
