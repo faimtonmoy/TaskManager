17dbb80c428ef5d028b67eb11a087cf79a82984c
Faim Tonmoy <bhuiyanfaim@gmail.com>
2021-06-26 00:34:13 +0600

    Cascading Delete System added

--- src/models/users.js
+++ src/models/users.js
@@ -2,6 +2,7 @@ const mongoose= require('mongoose')
 const validator= require('validator')
 const bcrypt= require('bcryptjs')
 const jwt= require('jsonwebtoken')
+const Task= require('./tasks')
 const userSchema= new mongoose.Schema({
     name:{
         type: String,
@@ -91,6 +92,9 @@ userSchema.pre('save', async function(next){
     next()
 
 })
-
+userSchema.pre('remove', async function(next){
+    await Task.deleteMany({Owner: this._id})
+    next()
+})
 const User= mongoose.model('User', userSchema)
 module.exports= User
